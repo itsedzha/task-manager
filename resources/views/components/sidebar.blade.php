16f687188d0251d223cc3b77d76e017ec0394391
Eduards Edgars Svars <svars.edgars@gmail.com>
2025-05-12 19:58:52 +0300

    salaboju lai rādas logout ikona sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -24,7 +24,9 @@
         <form method="POST" action="{{ route('logout') }}" id="logout-form">
             @csrf
             <a href="javascript:void(0)" onclick="document.getElementById('logout-form').submit();" class="flex items-center gap-2 hover:text-red-400">
-                <span class="material-symbols-outlined text-xl">logout</span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                </svg>
                 Log Out
             </a>
         </form>
